2ee3876258ff1c320acf18d5a427bfaf10fe69b8
Sinhs <pgrty45879@gmail.com>
2019-07-31 10:17:29 +0800

    Update lexer.py

--- pj/lexer.py
+++ pj/lexer.py
@@ -1,6 +1,5 @@
 from .constants import *
 
-JSON_QUOTE = '"'
 JSON_WHITESPACE = [' ', '\t', '\b', '\n', '\r']
 JSON_SYNTAX = [JSON_COMMA, JSON_COLON, JSON_LEFTBRACKET, JSON_RIGHTBRACKET,
                JSON_LEFTBRACE, JSON_RIGHTBRACE]
